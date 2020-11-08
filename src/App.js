db400a8cb5017b9d769f06bd2ce0b814d75e4210
luiizsilverio <luiiz.silverio@gmail.com>
2020-11-08 14:42:02 -0300

    Segundo desafio: React JS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,75 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositorios, setRepositorios] = useState([]);
+
+  // Chama a API (método GET, rota /projects)
+  useEffect(() => {
+    api.get('/repositories').then(resp => {
+        setRepositorios(resp.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const inputNew = document.getElementById('newProj')
+    let nome
+    
+    if (inputNew) {
+      nome = inputNew.value
+    } else {
+      nome = `Novo projeto ${Date.now()}`
+    } 
+
+    inputNew.value = ""
+
+    const response = await api.post('/repositories',  
+      {
+        //title: `${nome.value}`,
+        title: `${ nome }`, 
+        url: "http://localhost",
+        techs: ["html", "css", "javascript"]
+      })
+
+      setRepositorios([ ...repositorios, response.data])
+  }  
+
+  async function handleRemoveRepository(id) {    
+    await api.delete(`repositories/${ id }`)
+    
+    const indice = repositorios.findIndex(rep => rep.id === id)
+
+    if (indice >= 0) {
+      repositorios.splice(indice, 1);
+      setRepositorios([...repositorios]);
+    }    
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  function btnRemover(id) {
+    return (
+        <button onClick={() => handleRemoveRepository(id)}>
+            Remover
+        </button>
+    )
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <h2>Lista de Projetos</h2>
+      <ul data-testid="repository-list">             
+        { 
+          repositorios.map(rep => (
+            <li key={ rep.id }>{ rep.title }
+            { btnRemover(rep.id) }
+            </li>
+          ))
+        }
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <form className="form-add">
+        <input type="text" id="newProj" autoFocus autoComplete="off" placeholder="Digite o nome do projeto"/>
+        <button onClick={handleAddRepository} type="button">Adicionar</button>
+      </form>
     </div>
   );
 }
